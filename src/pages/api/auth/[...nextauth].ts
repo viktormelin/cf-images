c4eaf85083d3bfa304176d68d8655c0badd6d117
dough <viktor.melin@me.com>
2023-02-08 16:18:31 +0100

    refactor(typings): move types to separate file

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,6 +3,20 @@ import DiscordProvider from 'next-auth/providers/discord';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import prisma from '../../../lib/prisma';
 
+interface DiscordUser {
+	id: string;
+	username: string;
+	display_name?: string;
+	avatar: string;
+	avatar_decorations?: string;
+	discriminator: string;
+	public_flags: number;
+}
+interface DiscordRes {
+	roles: string[];
+	user: DiscordUser;
+}
+
 export default NextAuth({
 	adapter: PrismaAdapter(prisma),
 	providers: [
@@ -24,83 +38,77 @@ export default NextAuth({
 	callbacks: {
 		async signIn({ user, account }) {
 			const verifiedRoleId = '1064965680669135109';
-			let retVal: boolean | string = '/';
 
-			const dbUser = await prisma.user.findUnique({
-				where: {
-					id: user.id,
-				},
-			});
-
-			if (dbUser) {
-				if (account && account.access_token) {
-					await prisma.account.update({
-						where: {
-							provider_providerAccountId: {
-								provider: account.provider,
-								providerAccountId: account.providerAccountId,
-							},
-						},
-						data: {
-							access_token: account.access_token,
-							expires_at: account.expires_at,
-							id_token: account.id_token,
-							refresh_token: account.refresh_token,
-							session_state: account.session_state,
-							scope: account.scope,
-						},
-					});
-
-					await fetch(`https://discord.com/api/users/@me/guilds/${process.env.DISCORD_GUILD_ID}/member`, {
+			if (account) {
+				const response = await fetch(
+					`https://discord.com/api/users/@me/guilds/${process.env.DISCORD_GUILD_ID}/member`,
+					{
 						method: 'GET',
 						headers: {
-							Authorization: `Bearer ${account.access_token}`,
+							Authorization: `Bearer ${account?.access_token}`,
 						},
-					}).then((res) =>
-						res.json().then((data: { roles: string[] }) => {
-							if (data) {
-								if (data.roles.includes(verifiedRoleId)) {
-									user.roles = data.roles;
-									retVal = true;
-								}
-							}
-						})
-					);
+					}
+				);
+
+				if (response.status === 200) {
+					const resBody: DiscordRes = await response.json();
+
+					if (resBody.roles.includes(verifiedRoleId)) {
+						const dbUser = await prisma.user.findUnique({
+							where: { id: user.id },
+						});
+
+						if (dbUser) {
+							await prisma.account.update({
+								where: {
+									provider_providerAccountId: {
+										provider: account.provider,
+										providerAccountId: account.providerAccountId,
+									},
+								},
+								data: {
+									access_token: account.access_token,
+									expires_at: account.expires_at,
+									id_token: account.id_token,
+									refresh_token: account.refresh_token,
+									session_state: account.session_state,
+									scope: account.scope,
+								},
+							});
+						}
+
+						return true;
+					}
+
+					return false;
 				}
 			}
 
-			return retVal;
+			return false;
 		},
-		// async jwt({ token, account, profile }) {
-		// 	if (account) {
-		// 		token.accessToken = account.access_token;
+		// async session({ session, user }) {
+		// 	const account = await prisma.account.findFirst({
+		// 		where: {
+		// 			userId: user.id,
+		// 		},
+		// 	});
+
+		// 	if (account && account.access_token) {
+		// 		await fetch(`https://discord.com/api/users/@me/guilds/${process.env.DISCORD_GUILD_ID}/member`, {
+		// 			method: 'GET',
+		// 			headers: {
+		// 				Authorization: `Bearer ${account.access_token}`,
+		// 			},
+		// 		}).then((res) =>
+		// 			res.json().then((data: { roles: string[] }) => {
+		// 				if (data) {
+		// 					session.user.roles = data.roles;
+		// 				}
+		// 			})
+		// 		);
 		// 	}
 
-		// 	return token;
+		// 	return session;
 		// },
-		async session({ session, user }) {
-			const account = await prisma.account.findFirst({
-				where: {
-					userId: user.id,
-				},
-			});
-
-			if (account && account.access_token) {
-				await fetch(`https://discord.com/api/users/@me/guilds/${process.env.DISCORD_GUILD_ID}/member`, {
-					method: 'GET',
-					headers: {
-						Authorization: `Bearer ${account.access_token}`,
-					},
-				}).then((res) =>
-					res.json().then((data: { roles: string[] }) => {
-						if (data) {
-							session.user.roles = data.roles;
-						}
-					})
-				);
-			}
-
-			return session;
-		},
 	},
 });
