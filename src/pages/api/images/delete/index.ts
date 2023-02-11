5274160fa108b68493e62f00c49f376e8bf0743c
dough <viktor.melin@me.com>
2023-02-11 19:40:10 +0100

    feat(api): create route to delete images

---
+++ src/pages/api/images/delete/index.ts
@@ -0,0 +1,34 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '@/lib/prisma';
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+	const { image } = req.query;
+
+	const sessionToken = req.cookies['next-auth.session-token'];
+
+	// Check if authenticated
+	if (image && sessionToken) {
+		const user = await prisma.session.findUnique({ where: { sessionToken } });
+
+		if (user) {
+			const deletedImage = await prisma.images.deleteMany({
+				where: {
+					userId: user.userId,
+					imageId: image as string,
+				},
+			});
+
+			if (deletedImage) {
+				res.status(204).end();
+			} else {
+				res.status(500).end();
+			}
+		} else {
+			res.status(500).send('Failed to find user from session');
+		}
+	} else {
+		res.status(401).end();
+	}
+};
+
+export default handler;
