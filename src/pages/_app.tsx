9282c89dedec9f0cdb4533533bda762a0d11c523
dough <viktor.melin@me.com>
2023-02-11 19:39:11 +0100

    perf(query): change query state

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,27 +1,30 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { SessionProvider } from 'next-auth/react';
-import { QueryClient, QueryClientProvider } from 'react-query';
+import type { DehydratedState } from 'react-query';
+import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 import { MantineProvider } from '@mantine/core';
 import { ModalsProvider } from '@mantine/modals';
 import { NotificationsProvider } from '@mantine/notifications';
 import theme from '@/themes/theme';
 import type { Session } from 'next-auth';
+import { useState } from 'react';
 
-const queryClient = new QueryClient();
-
-export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+export default function App({ Component, pageProps: { session, dehydratedState, ...pageProps } }: AppProps) {
+	const [queryClient] = useState(() => new QueryClient());
 	return (
 		<QueryClientProvider client={queryClient}>
-			<MantineProvider theme={theme}>
-				<ModalsProvider>
-					<NotificationsProvider>
-						<SessionProvider session={session as Session}>
-							<Component {...pageProps} />
-						</SessionProvider>
-					</NotificationsProvider>
-				</ModalsProvider>
-			</MantineProvider>
+			<Hydrate state={dehydratedState as DehydratedState}>
+				<MantineProvider theme={theme}>
+					<ModalsProvider>
+						<NotificationsProvider>
+							<SessionProvider session={session as Session}>
+								<Component {...pageProps} />
+							</SessionProvider>
+						</NotificationsProvider>
+					</ModalsProvider>
+				</MantineProvider>
+			</Hydrate>
 		</QueryClientProvider>
 	);
 }
